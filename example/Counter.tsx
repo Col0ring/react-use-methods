7b713e19dabc699284a5ce5a1c6cde515555967e
Col0ring <1561999073@qq.com>
2021-07-29 10:30:31 +0800

    fix: fix example

--- example/Counter.tsx
+++ example/Counter.tsx
@@ -1,58 +1,44 @@
-import { createUseMethods } from '@packages/index'
+import { useMethods } from '@packages/index'
 import { combineReducers } from '@packages/reducer-mapper/immer'
 import React from 'react'
-import thunk from 'redux-thunk'
 
-const useMethods = createUseMethods(
-  {
-    // use immer
-    reducerMapper: combineReducers,
-  },
-  thunk
-)
+interface MethodsState {
+  count: number
+}
 
-function App() {
+const Counter: React.FC = () => {
   const [{ count }, methods] = useMethods(
     (state) => {
       return [
         {
           increment() {
             state.count += 1
+            // 这里可以不导出也不会报错，但是 ts 会报错
             return state
+          },
+          // 支持异步
+          async incrementDouble() {
+            // 异步不要使用 immer
+            // state.count *= 2
+            return { ...state, count: state.count * 2 }
           },
           decrement() {
             state.count -= 1
             return state
           },
+          set(current: number) {
+            // 直接返回
+            return { ...state, count: current }
+          },
           reset() {
-            state.count = 0
-            return state
+            return { ...state, count: 0 }
           },
-          addAndReset() {
-            return ({ dispatch }) => {
-              const addAndReset = () => {
-                return (thunkDispatch: typeof dispatch) => {
-                  thunkDispatch({ type: 'increment' })
-                  setTimeout(() => {
-                    thunkDispatch({ type: 'reset' })
-                  }, 1000)
-                }
-              }
-              dispatch(addAndReset())
-            }
-          },
-        },
-        {
-          async count(dispatch, newValue, oldValue) {
-            console.log(newValue, oldValue)
-            if (newValue < 0) {
+          midReset() {
+            return async ({ dispatch, payload }) => {
+              // 可在这里使用诸如 redux-thunk 这样的中间件
               dispatch({
-                type: 'increment',
-              })
-            }
-            if (newValue > 10) {
-              dispatch({
-                type: 'addAndReset',
+                type: 'reset',
+                payload,
               })
             }
           },
@@ -61,16 +47,22 @@ function App() {
     },
     {
       count: 0,
-      ss: '1',
+    } as MethodsState,
+    {
+      reducerMapper: combineReducers,
     }
   )
   return (
     <div>
       {count}
       <button onClick={methods.increment}>increment</button>
+      <button onClick={methods.incrementDouble}>incrementDouble</button>
       <button onClick={methods.decrement}>decrement</button>
-      <button onClick={methods.addAndReset}>addAndReset</button>
+      <button onClick={() => methods.set(10)}>set 10</button>
+      <button onClick={() => methods.reset()}>reset</button>
+      <button onClick={() => methods.midReset()}>midReset</button>
     </div>
   )
 }
-export default App
+
+export default Counter

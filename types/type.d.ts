9ba9a1595b072b9240d6d9b8624c6f61b224176f
栗嘉男 <574980606@qq.com>
2021-12-11 23:00:26 +0800

    type: update the Key

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,4 +1,4 @@
-export declare type Key = string | number | symbol
+export declare type Key = PropertyKey
 export declare type ResolvePromise<T> = T extends Promise<infer U> ? U : T
 export interface AnyAction<T = any> {
   type: T
